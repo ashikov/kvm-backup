a89331134ba60c77691e9391d888f62ec08e4f69
ashikov <rmn.shkv@gmail.com>
2019-08-07 11:20:39 +0700

    added compression and send to backup server commands

--- kvm-guest-backup.sh
+++ kvm-guest-backup.sh
@@ -8,6 +8,12 @@ DOMAIN_DIR="/mnt/md0/kvm"
 MAIN_BACKUP_DIR="/mnt/backup_sde1/kvm/win2k8r2"
 CURRENT_BACKUP_DIR=$MAIN_BACKUP_DIR/$CURRENT_DATE
 
+# backup server config
+BACKUP_SERVER_IP="x.x.x.x"
+SSH_USER="x"
+SSH_PASSWORD="x"
+REMOTE_BACKUP_DIR="guests/$VM_NAME"
+
 mkdir $CURRENT_BACKUP_DIR
 
 counter=0
@@ -33,14 +39,23 @@ echo "$DOMAIN_DIR/$VM_NAME.qcow2" >> $CURRENT_BACKUP_DIR/$VM_NAME
 echo "metadata successfully saved"
 
 echo "compressing disk file"
-
 dd if=$DOMAIN_DIR/$VM_NAME.qcow2 | gzip -kc -3 > $CURRENT_BACKUP_DIR/$VM_NAME.qcow2.gz
-
 echo "Done"
 
 virsh start $VM_NAME
 
 echo "$VM_NAME is $VM_STATUS"
 
+echo "create archive file from backup directory"
+tar -cvf $MAIN_BACKUP_DIR/$CURRENT_DATE.tar.gz $CURRENT_BACKUP_DIR/
+
+echo "sending data to backup-server"
+sshpass -p "$SSH_PASSWORD" scp $MAIN_BACKUP_DIR/$CURRENT_DATE.tar.gz $SSH_USER@$BACKUP_SERVER_IP:~/guests/$VM_NAME/
+
+echo "deleting current backup from local storage"
+rm -rvf $CURRENT_BACKUP_DIR
+rm -rvf $MAIN_BACKUP_DIR/$CURRENT_DATE.tar.gz
+
 echo "backup completed"
+
 
