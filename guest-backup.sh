7eee2bb613f4c825ac13c48e4083bccc928f9244
ashikov <rmn.shkv@gmail.com>
2019-10-15 09:44:35 +0700

    shellcheck fixes

--- guest-backup.sh
+++ guest-backup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CURRENT_DATE=`date +"%Y%m%d_%H%M%S"`
+CURRENT_DATE=$(date +"%H%M%S_%d%m%Y")
 VM_NAME="win2k8r2"
-VM_STATUS=`virsh domstate $VM_NAME`
+VM_STATUS=$(virsh domstate $VM_NAME)
 DOMAIN_DIR="/mnt/md0/kvm"
 MAIN_BACKUP_DIR="/mnt/backup_sde1/kvm/$VM_NAME"
 CURRENT_BACKUP_DIR=$MAIN_BACKUP_DIR/$CURRENT_DATE
@@ -12,18 +12,18 @@ BACKUP_SERVER_IP="x.x.x.x"
 SSH_USER="x"
 REMOTE_BACKUP_DIR="guests/$VM_NAME"
 
-mkdir $CURRENT_BACKUP_DIR
+mkdir CURRENT_BACKUP_DIR
 
 counter=0
 while [ "$VM_STATUS" == "running" ]; do
   echo "$VM_NAME is $VM_STATUS"
   virsh shutdown $VM_NAME
   sleep 10
-  VM_STATUS=`virsh domstate $VM_NAME`
+  VM_STATUS=$(virsh domstate $VM_NAME)
 
   counter=$((counter + 1)) # sending email if its runnig too long
-  if [ counter == 10 ]; then
-    echo -e "Subject: KVM backup error - IO\n\nVM $VM_NAME does not shutdown" | ssmtp errors@tdom.info
+  if [ $counter == 10 ]; then
+    echo -e "Subject: KVM backup error - IO\\n\\nVM $VM_NAME does not shutdown" | ssmtp errors@tdom.info
     break
   fi
 done
@@ -55,5 +55,3 @@ rm -rvf $CURRENT_BACKUP_DIR
 rm -rvf $MAIN_BACKUP_DIR/$CURRENT_DATE.tar.gz
 
 echo "backup completed"
-
-
