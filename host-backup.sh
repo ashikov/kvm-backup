d61b88676b68c48c7c1ef64988599614be49919d
ashikov <rmn.shkv@gmail.com>
2019-11-01 17:46:31 +0700

    some fixes

--- host-backup.sh
+++ host-backup.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-CURRENT_DATE=`date +"%Y%m%d_%H%M%S"`
-MAIN_BACKUP_DIR="/mnt/backup_sde1"
+CURRENT_DATE=$(date +"%H%M%S_%d%m%Y")
+BACKUP_DIR="/mnt/backup_sde1"
 
 # backup server config
 BACKUP_SERVER_IP="x.x.x.x"
 SSH_USER="x"
 REMOTE_BACKUP_DIR="host"
 
-echo "hello, bro!"
-echo "compressing system files"
-sudo tar cvpzf $MAIN_BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt --exclude=/sys /
+printf "Compressing system files\\n\\n"
+sudo tar cvpzf "$BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz" --exclude=/home --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/tmp / > /dev/null
 
-echo "sending data to backup-server"
-scp $MAIN_BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz $SSH_USER@$BACKUP_SERVER_IP:~/$REMOTE_BACKUP_DIR/
+printf "Sending data to backup-server\\n\\n"
+scp "$BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz" $SSH_USER@$BACKUP_SERVER_IP:~/$REMOTE_BACKUP_DIR/
 
-echo "deleting current backup from local storage"
-rm -rvf $MAIN_BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz
+printf "Deleting current backup from local storage\\n\\n"
+rm -rvf "$BACKUP_DIR/ubuntu_server_$CURRENT_DATE.tgz"
 
-echo "backup completed"
+printf "Completed\\n\\n"
