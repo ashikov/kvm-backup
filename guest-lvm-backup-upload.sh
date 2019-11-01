36e218fe87a676e5f188fb38a18ed77a0d880ba6
ashikov <rmn.shkv@gmail.com>
2019-11-01 12:06:29 +0700

    comlition guest lvm backup script with upload on ftp

--- guest-lvm-backup-upload.sh
+++ guest-lvm-backup-upload.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 CURRENT_DATE=$(date +"%H%M%S_%d%m%Y")
-VM_NAME="kvmserv-a-1"
+VM_NAME="kvmserv-io-1"
 BACKUP_DIR="/mnt/backups"
-LV_NAME="kvmserv-a-1-lvm-disk-1"
-LV_PATH="/dev/vg-libvirt/kvmserv-a-1-lvm-disk-1"
-COW_TABLE_SIZE="32G"
+LV_NAME="kvmserv-io-1-lvm-disk-1"
+LV_PATH="/dev/vg1-kvm/kvmserv-io-1-lvm-disk-1"
+COW_TABLE_SIZE="20G"
+
+# Backup-server config
+BACKUP_SERVER_IP="ip-address"
+SSH_USER="username"
+REMOTE_BACKUP_DIR="guests/$VM_NAME"
 
 printf "\\nSaving disk metadata and XML-config\\n\\n"
 virsh domblkinfo $VM_NAME $LV_PATH > "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME"
@@ -14,7 +19,6 @@ echo "$LV_PATH" >> "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME"
 printf "Metadata successfully saved\\n\\n"
 virsh dumpxml $VM_NAME > "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME.xml"
 printf "XML-config successfully saved\\n\\n"
-
 
 printf "Saving VM state\\n"
 virsh save $VM_NAME "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME.vmstate" --running
@@ -30,3 +34,16 @@ printf "Compressing snapshot\\n\\n"
 dd if="$LV_PATH"_snap | gzip -ck -3 > "$BACKUP_DIR/$CURRENT_DATE-$LV_NAME.gz"
 printf "Done\\n\\n"
 
+printf "\\nRemoving snapshot\\n\\n"
+lvremove --force "$LV_PATH"_snap
+
+printf "Packing backup files in tar\\n\\n"
+tar -cvf "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME.tar.gz" $BACKUP_DIR/
+
+printf "Sending data to backup-server\\n\\n"
+scp "$BACKUP_DIR/$CURRENT_DATE-$VM_NAME.tar.gz" $SSH_USER@$BACKUP_SERVER_IP:~/$REMOTE_BACKUP_DIR/
+
+printf "Deleting current backup files from local storage\\n\\n"
+rm -rvf "${BACKUP_DIR:?}/"*
+
+printf "\\nBackup completed\\n\\n"
